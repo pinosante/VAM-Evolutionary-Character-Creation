61389c983ed1ce9adf3f534b0fbab305d873cd19
chris-b-pacon <this_is_not@my_real_address.com>
2023-01-30 19:08:14 +0100

    Add unit test file for ecc_logic with first test for can_match_genders

---
+++ test_logic.py
@@ -0,0 +1,32 @@
+import unittest
+import ecc_logic
+
+
+class TestLogicMethods(unittest.TestCase):
+    def test_can_match_genders(self):
+        m = 'Male'
+        f = 'Female'
+        fu = 'Futa'
+        test_cases = [
+            # gender1, gender2, expected result
+            (m, m, True),
+            (f, f, True),
+            (fu, fu, True),
+            (m, f, False),
+            (f, m, False),
+            (m, fu, False),
+            (fu, m, False),
+            (f, fu, True),
+            (fu, f, True)
+        ]
+
+        for test_case in test_cases:
+            g1 = test_case[0]
+            g2 = test_case[1]
+            expected_result = test_case[2]
+            result = ecc_logic.can_match_genders(g1, g2)
+            self.assertTrue(expected_result == result)
+
+
+if __name__ == '__main__':
+    unittest.main()
