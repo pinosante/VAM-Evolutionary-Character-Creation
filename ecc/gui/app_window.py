7082f630c7cebdfa5545a034920a0e495e5cefff
chris-b-pacon <this_is_not@my_real_address.com>
2023-03-17 17:57:19 +0100

    Moving frames into separate classes

--- ecc/gui/app_window.py
+++ ecc/gui/app_window.py
@@ -43,6 +43,23 @@ CHOOSE_FILES_TEXT = "Choose Files"
 # RATING_ACTIVE_BG_COLOR = BUTTON_BG_COLOR
 # RATING_ACTIVE_FG_COLOR = BUTTON_FG_COLOR
 
+class MethodFrame(tk.Frame):
+    def __init__(self, padding, font, gaussian_strategy, random_crossover_strategy):
+        super().__init__()
+
+        #self.method_frame = tk.Frame(self.master, bg=BG_COLOR)
+        self.method_label = tk.Label(self, text="Step 6: Initialization Method", font=font,
+                                     bg=BG_COLOR, fg=FG_COLOR)
+        self.method_label.grid(columnspan=2, row=0, column=0, sticky=tk.W, pady=(0, 0))
+        self.gaussian_button = tk.Button(self, text="Gaussian Samples", bg=BUTTON_BG_COLOR,
+                                         fg=BUTTON_FG_COLOR, activebackground=BUTTON_ACTIVE_COLOR,
+                                         command=lambda: gaussian_strategy())
+        self.gaussian_button.grid(row=1, column=0, sticky=tk.W)
+        self.random_cross_over_button = tk.Button(self, text="Random Crossover", bg=BUTTON_BG_COLOR,
+                                                  fg=BUTTON_FG_COLOR, activebackground=BUTTON_ACTIVE_COLOR,
+                                                  command=lambda: random_crossover_strategy())
+        self.random_cross_over_button.grid(row=1, column=1, sticky=tk.W)
+
 
 class AppWindow(tk.Frame):
     def __init__(self, settings, generator):
@@ -78,10 +95,6 @@ class AppWindow(tk.Frame):
         self.favorites_info = None
         self.favorites_label = None
         self.favorites_frame = None
-        self.random_cross_over_button = None
-        self.gaussian_button = None
-        self.method_label = None
-        self.method_frame = None
         self.recursive_directory_search_no_button = None
         self.recursive_directory_search_yes_button = None
         self.recursive_directory_search_label = None
@@ -104,6 +117,7 @@ class AppWindow(tk.Frame):
         self.subtitle_font = (DEFAULT_FONT, 11, "bold")
         self.subtitle_padding = 1
 
+
         # create a dictionary to select appearances
         self.select_appearances_strategies = {
             CHOOSE_ALL_FAVORITES_TEXT: lambda: self.get_fav_appearance_filenames(),
@@ -120,7 +134,11 @@ class AppWindow(tk.Frame):
         self.init_parent_files_frame()
         self.init_chromosome_list_frame(settings)
         self.init_alternative_appearances_frame()
-        self.init_initialization_method()
+        self.method_frame = MethodFrame(self.subtitle_font, self.subtitle_padding,
+                                        self.choose_gaussian_samples, self.choose_random_crossover)
+        self.method_frame.grid(row=6, column=1, padx=10, pady=self.subtitle_padding, sticky=tk.W)
+
+        #self.init_initialization_method()
         self.init_options_frame()
         self.init_generate_button()
 
@@ -590,16 +608,16 @@ Do you want to continue that session?""")
     def choose_gaussian_samples(self):
         """ Called when the Gaussian Samples button is pressed. Raises the random crossover button. Saves choice in
             settings. """
-        self.gaussian_button.configure(relief=tk.SUNKEN)
-        self.random_cross_over_button.configure(relief=tk.RAISED)
+        self.method_frame.gaussian_button.configure(relief=tk.SUNKEN)
+        self.method_frame.random_cross_over_button.configure(relief=tk.RAISED)
         self.settings['method'] = 'Gaussian Samples'
         self.update_initialize_population_button()
 
     def choose_random_crossover(self):
         """ Called when the random crossover button is pressed. Raises the Gaussian Samples  button. Saves choice in
             settings. """
-        self.gaussian_button.configure(relief=tk.RAISED)
-        self.random_cross_over_button.configure(relief=tk.SUNKEN)
+        self.method_frame.gaussian_button.configure(relief=tk.RAISED)
+        self.method_frame.random_cross_over_button.configure(relief=tk.SUNKEN)
         self.settings['method'] = 'Random Crossover'
         self.update_initialize_population_button()
 
